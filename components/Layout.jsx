d5f5598d4c5a4a673e74dba9dafad15447aff4ae
Israeltheminer <israeliyanda5@gmail.com>
2022-10-04 15:30:54 +0100

    Edit general styling

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,21 @@
 import React from "react"
+import Head from "next/head"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
 
-const Layout = () => {
-	return <div>Layout</div>
+const Layout = ({ children }) => {
+	return (
+		<div>
+			<Head>
+				<title>Agora</title>
+			</Head>
+			<header className='px-[3.5%] py-3 border-b border-[#eee]'>
+				<Navbar />
+			</header>
+			<main className='main-container px-[6%]'>{children}</main>
+			<Footer />
+		</div>
+	)
 }
 
 export default Layout
