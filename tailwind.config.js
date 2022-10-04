f71ab85db79b49856111600008f2c5f989656a07
Israeltheminer <israeliyanda5@gmail.com>
2022-10-04 15:11:50 +0100

    Add screen property to enable reponsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
 	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
 	theme: {
+		screens: {
+			xl: { max: "1500px" },
+			lg: { max: "1300px" },
+			md: { max: "1000px" },
+			sm: { max: "650px" },
+			xs: { max: "375px" }
+		},
 		extend: {}
 	},
 	plugins: []
