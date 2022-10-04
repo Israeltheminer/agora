9ac951cd5d3269aeab27c40e0d05301fcb23c7dd
Israeltheminer <israeliyanda5@gmail.com>
2022-10-04 15:16:38 +0100

    Structure footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,18 @@
 import React from "react"
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai"
+import { FaFacebookF } from "react-icons/fa"
 
 const Footer = () => {
-	return <div>Footer</div>
+	return (
+		<div className='footer-container'>
+			<p>&copy;2022 Agora, Inc. All Rights Reserved</p>
+			<p className='icons mt-4'>
+				<AiFillInstagram />
+				<AiOutlineTwitter />
+				<FaFacebookF />
+			</p>
+		</div>
+	)
 }
 
 export default Footer
