bf8384e8df4dad021d6f868c5cb6f7456b858d15
Israeltheminer <israeliyanda5@gmail.com>
2022-10-04 15:19:42 +0100

    Structure product section component

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,7 +1,24 @@
 import React from "react"
+import Link from "next/link"
+import Image from "next/image"
+import { urlFor } from "../lib/client"
 
-const Product = () => {
-	return <div>Product</div>
+const Product = ({ product: { image, name, slug, price } }) => {
+	return (
+		<div className=''>
+			<Link href={`/product/${slug.current}`}>
+				<div className='product-card'>
+					<div className='hover:scale-[1.1] transition ease-in-out duration-300 mb-6 grid justify-center'>
+						<div className='product-image'>
+							{urlFor(image) && <Image src={`${urlFor(image[0])}`} alt={slug} layout='fill' objectFit='cover' />}
+						</div>
+					</div>
+				</div>
+			</Link>
+			<p className='product-name opacity-60'>{name}</p>
+			<p className='product-price opacity-80'>${price}</p>
+		</div>
+	)
 }
 
 export default Product
