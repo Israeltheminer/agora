ca1639428624259e25f94c9360751d84c3f8050b
Israeltheminer <israeliyanda5@gmail.com>
2022-10-04 15:21:00 +0100

    Connect components to main page

--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,36 @@ import React from "react"
 import { Cart, FooterBanner, HeroBanner, Layout, Product, Navbar } from "../components"
 import { client } from "../lib/client.js"
 
-const index = ({ productData, bannerData }) => {
+const index = ({ productData, heroBannerData, bottomBannerData }) => {
 	return (
 		<>
-			<div className='smooth-scroll px-[6%]'>
-				<HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+			<div className='smooth-scroll'>
+				<HeroBanner heroBanner={heroBannerData.length && heroBannerData[0]} />
 				<div className='products-heading mt-20'>
 					<h2 className='text-xl'>Shoes & Sneakers</h2>
-					<p className='mt-4'>Nike | Adidas | Yeezy | Converse</p>
+					<p className='mt-4 opacity-80'>Nike | Adidas | Yeezy | Converse</p>
 				</div>
-				<div className='products-container grid grid-cols-3'>
-					{console.log(productData)}
+				<div className='products-container grid grid-cols-4 xl:grid-cols-3 lg:grid-cols-2'>
 					{productData?.map((product) => (
 						<Product key={product._id} product={product} />
 					))}
 				</div>
-				<FooterBanner />
+				<FooterBanner bottomBanner={bottomBannerData.length && bottomBannerData[0]} />
 			</div>
 		</>
 	)
 }
 
 const productQuery = `*[_type == "product"] | order(_createdAt desc)`
-const bannerQuery = `*[_type == "banner"] | order(_createdAt desc)`
+const heroBannerQuery = `*[_type == "banner"] | order(_createdAt desc)`
+const bottomBannerQuery = `*[_type == "bottomBanner"] | order(_createdAt desc)`
 
 export async function getStaticProps() {
 	const productData = await client.fetch(productQuery)
-	const bannerData = await client.fetch(bannerQuery)
+	const heroBannerData = await client.fetch(heroBannerQuery)
+	const bottomBannerData = await client.fetch(bottomBannerQuery)
 	return {
-		props: { productData, bannerData }
+		props: { productData, heroBannerData, bottomBannerData }
 	}
 }
 
