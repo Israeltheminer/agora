ab5fbe3048b6f9ef780e7ac2acfa6cbc37e2704d
Israeltheminer <israeliyanda5@gmail.com>
2022-10-04 15:15:14 +0100

    Add bottom banner schema to sanity project

---
+++ agora-studio/schemas/bottom-banner.js
@@ -0,0 +1,55 @@
+export default {
+	name: "bottomBanner",
+	title: "Bottom Banner",
+	type: "document",
+	fields: [
+		{
+			name: "image",
+			title: "Image",
+			type: "image",
+			options: {
+				hotspot: true
+			}
+		},
+		{
+			name: "product",
+			title: "Product",
+			type: "string"
+		},
+		{
+			name: "buttonText",
+			title: "Button Text",
+			type: "string"
+		},
+		{
+			name: "description",
+			title: "Desc",
+			type: "string"
+		},
+		{
+			name: "discount",
+			title: "Discount",
+			type: "string"
+		},
+		{
+			name: "saleTime",
+			title: "Sale Time",
+			type: "string"
+		},
+		{
+			name: "largeText1",
+			title: "Large Text 1",
+			type: "string"
+		},
+		{
+			name: "midText",
+			title: "Mid Text",
+			type: "string"
+		},
+		{
+			name: "smallText",
+			title: "Small Text",
+			type: "string"
+		}
+	]
+}
