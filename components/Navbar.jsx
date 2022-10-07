89d7f14ea34b36d012d38aba28fc24715b571d53
Israeltheminer <israeliyanda5@gmail.com>
2022-10-07 13:37:49 +0100

    include states and state management functions for cart component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,11 @@ import React from "react"
 import { AiOutlineShoppingCart } from "react-icons/ai"
 import { BiBookmarkHeart } from "react-icons/bi"
 import { GiHamburgerMenu } from "react-icons/gi"
+import { useStateContext } from "../context/StateContext"
+import Cart from "./Cart"
+
 const Navbar = () => {
+	const { cartItems, showCart, totalQuantity, totalPrice, toogleCartVisibility } = useStateContext()
 	return (
 		<div className='navbar-container bg-white'>
 			<p className='nav-text font-bold logo text-xl'>
@@ -16,11 +20,30 @@ const Navbar = () => {
 				<button type='button' className='navbar-icon sm:hidden' title='Wishlist'>
 					<BiBookmarkHeart />
 				</button>
-				<button type='button' className='navbar-icon' title='Cart'>
-					<AiOutlineShoppingCart />
-					<span className='cart-item-qty'>1</span>
-				</button>
-				<button type='button' className='navbar-icon hidden sm:inline-block' title='Wishlist'>
+				<div className='relative flex items-center'>
+					<button type='button' className='navbar-icon relative' title='Cart' onClick={() => toogleCartVisibility()}>
+						<div className=''>
+							<AiOutlineShoppingCart />
+						</div>
+						{totalQuantity > 0 && <span className='cart-item-qty'>{totalQuantity}</span>}
+					</button>
+					{showCart && totalQuantity > 0 && (
+						<div className=' absolute top-8 right-6 bg-white shadow-lg z-50 rounded-xl w-80'>
+							<Cart cartItems={cartItems} totalPrice={totalPrice} />
+						</div>
+					)}
+					{showCart && totalQuantity == 0 && (
+						<div className=' absolute top-8 right-6 bg-white z-50 shadow-2xl rounded-xl w-80 py-16 text-[#1f79e0]'>
+							<div className='flex justify-center items-center text-[90px]'>
+								<AiOutlineShoppingCart />
+							</div>
+							<span className=''>
+								<p className='font-bold text-lg text-center mt-3'>Your shopping cart is empty</p>
+							</span>
+						</div>
+					)}
+				</div>
+				<button type='button' className='navbar-icon hidden sm:inline-block cursor-pointer'>
 					<GiHamburgerMenu />
 				</button>
 			</div>
