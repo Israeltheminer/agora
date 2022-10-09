c2bd3d6dc7ecec87793240690ad1c521b0e88649
Israeltheminer <israeliyanda5@gmail.com>
2022-10-09 04:30:09 +0100

    fix deployment bug

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import React, { useRef } from "react"
+import React from "react"
 import { Cart, FooterBanner, HeroBanner, Layout, Product, Navbar } from "../components"
 import { client } from "../lib/client.js"
-import { motion, useInView } from "framer-motion"
+import { motion } from "framer-motion"
 
 const index = ({ productData, heroBannerData, bottomBannerData }) => {
-	const productsRef = useRef(null)
-	const productsInView = useInView(productsRef)
 	return (
 		<>
 			<div className=''>
@@ -17,18 +15,16 @@ const index = ({ productData, heroBannerData, bottomBannerData }) => {
 						<h2 className='text-3xl xs:text-[24px]'>Shoes & Sneakers</h2>
 						<p className='mt-4 opacity-80 xs:text-[14px]'>Nike | Adidas | Yeezy | Converse</p>
 					</div>
-					<motion.div ref={productsRef}>
-						<motion.div
-							className='products-container grid grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 sm:grid-cols-1'
-							animate={productsInView ? { scale: 1 } : { scale: 0.6 }}
-							initial={productsInView ? { scale: 0.6 } : { scale: 1 }}
-							transition={{ type: "spring", stiffness: 50, staggerChildren: 0.5, delayChildren: 1, when: "beforeChildren" }}>
-							{productData?.map((product) => (
-								<motion.div key={product._id} animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
-									<Product product={product} />
-								</motion.div>
-							))}
-						</motion.div>
+					<motion.div
+						className='products-container grid grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 sm:grid-cols-1'
+						whileInView={{ scale: 1 }}
+						initial={{ scale: 0.6 }}
+						transition={{ type: "spring", stiffness: 50, staggerChildren: 0.5, delayChildren: 1, when: "beforeChildren" }}>
+						{productData?.map((product) => (
+							<motion.div key={product._id} animate={{ opacity: 1 }} initial={{ opacity: 0 }}>
+								<Product product={product} />
+							</motion.div>
+						))}
 					</motion.div>
 				</div>
 				<div className='px-[6%]'>
